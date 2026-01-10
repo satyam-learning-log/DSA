05c535335d733b90974e9cd5f8a3b1f15e40995d
Satyam Kodale <satyamkodale38@gmail.com>
2026-01-10 12:46:54 +0530

    Solved Leetcode 547 L547_Number_of_ProvincesxConnectedComponents

--- DSA_TOPICS/Graph/L547_Number_of_ProvincesxConnectedComponents.java
+++ DSA_TOPICS/Graph/L547_Number_of_ProvincesxConnectedComponents.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConnectedComponents {
+public class L547_Number_of_ProvincesxConnectedComponents {
 
     static List<List<Integer>> adjacencyList = new ArrayList<>();
 
-    public static void buildAdjacencyList(int edges[][]){
+    private static void buildAdjacencyList(int edges[][]){
         for(int i=0; i<6; i++){
             adjacencyList.add(new ArrayList<>());
         }
@@ -14,11 +14,17 @@ public class ConnectedComponents {
             adjacencyList.get(edges[i][1]).add(edges[i][0]);
         }
     }
-    public static void main(String [] args){
+    public static void main(String[] args){
+        System.out.println("Example of Connected components using Adjacency List");
         int n = 6;
         int edges [][] = {{0,1},{1,2},{2,3},{4,5}};
         buildAdjacencyList(edges);
         System.out.println(countAndReturnConnectedComponents(edges,adjacencyList,n));
+
+        System.out.println("Example of Connected components using Adjacency Matrix and Traversal on it directly");
+        int [][] isConnected = {{1,1,1,0,0},{1,1,1,0,0},{1,1,1,0,0},{0,0,0,1,1},{0,0,0,1,1}};
+        System.out.println(findCircleNum( isConnected));
+
     }
 
     private static int countAndReturnConnectedComponents(int[][] edges, List<List<Integer>> adjacencyMatric , int n) {
@@ -33,6 +39,33 @@ public class ConnectedComponents {
         }
 
         return count;
+    }
+
+    private static int findCircleNum(int[][] isConnected) {
+
+        int visited[] = new int[isConnected.length];
+
+        int count = 0;
+
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i] != 1) {
+                visit(i, visited, isConnected);
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+
+    private static void visit(int start , int visited [] , int[][] isConnected ){
+        visited[start]=1;
+
+        for(int j=0; j<isConnected.length; j++){
+            if(isConnected[start][j] ==1  && visited[j]!=1 && start !=j ){
+                visit(j,visited,isConnected);
+            }
+        }
     }
 
     private static void visit(int start , int [] visited ) {
