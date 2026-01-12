044743d80a0d57e130b8b9535e5b9f43db559e99
Satyam Kodale <satyamkodale38@gmail.com>
2026-01-12 08:57:54 +0530

    revisied L144_Binary_Tree_Preorder_Traversal

---
+++ DSA_TOPICS/Tree/L144_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+     TreeNode left;
+     TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+
+public class L144_Binary_Tree_Preorder_Traversal {
+
+    public void preorderByMorrisTraversal(TreeNode root){
+        //aim ? -> for every current node attach currentleft's right most to current's right
+
+        TreeNode current = root;
+
+        while(current!=null){
+            System.out.println(current.val);
+
+            if(current.left == null){
+                current = current.right;
+            }else{
+                //attach currentleft's right most to current's right
+                TreeNode currentLeft = current.left;
+
+                while(currentLeft.right!=null){
+                    currentLeft = currentLeft.right;
+                }
+                currentLeft.right = current.right;
+                current = current.left;
+            }
+        }
+    }
+
+    // PREORDER BY RECURRSION
+     public void preorderByRecurrsion(TreeNode root, List<Integer> list)
+     {
+         if(root==null)
+         {
+             return;
+         }
+         list.add(root.val);
+         preorderByRecurrsion(root.left,list);
+         preorderByRecurrsion(root.right,list);
+     }
+
+    // PREORDER BY ITERATION
+     public List<Integer> preorderbyIteration(TreeNode root ){
+
+         List<Integer> list = new ArrayList<>();
+          if (root == null) {
+              return list;
+          }
+
+          Stack<TreeNode> st = new Stack<>();
+          st.push(root);
+
+          while (!st.isEmpty()) {
+              TreeNode top = st.pop();
+
+              // Add the current node's value to the result list
+              list.add(top.val);
+
+              // Push right and left children to stack if they are not null
+              if (top.right != null) {
+                  st.push(top.right);
+              }
+              if (top.left != null) {
+                  st.push(top.left);
+              }
+          }
+
+          return list;
+     }
+}
