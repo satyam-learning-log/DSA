e7dcbadc108cb8bac6e6068c3c5b2daf221abb61
Satyam Kodale <satyamkodale38@gmail.com>
2026-01-13 08:48:28 +0530

    solved AZ_Detect_Cycle_In_Graph_DFS.java

---
+++ DSA_TOPICS/Graph/ProblemsOnBFSAndDFS/AZ_Detect_Cycle_In_Graph_DFS.java
@@ -0,0 +1,44 @@
+package ProblemsOnBFSAndDFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AZ_Detect_Cycle_In_Graph_DFS {
+    static List<List<Integer>> adjacencyList = new ArrayList<>();
+
+    public static void buildAdjacencyList(int edges[][]){
+
+        for(int i=0; i<8; i++){
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<edges.length; i++){
+            adjacencyList.get(edges[i][0]).add(edges[i][1]);
+            adjacencyList.get(edges[i][1]).add(edges[i][0]);
+        }
+
+    }
+    public static boolean detectCycleInGraph(int node , int parent , int visited[] , List<List<Integer>> adjacencyList ){
+        List<Integer> neighbors = adjacencyList.get(node);
+        visited[node]=1;
+        for(int i=0; i<neighbors.size(); i++){
+           if(visited[neighbors.get(i)]!=1){
+               if(detectCycleInGraph(neighbors.get(i) ,  node , visited ,  adjacencyList )) return true;
+           }else if(neighbors.get(i)!=parent){
+               return true;
+           }
+        }
+       return false;
+    }
+    public static void main(String[] args){
+        int edges [][] = {{1,2},{1,3},{3,4},{3,6},{2,5},{5,7},{6,7}};
+        int visited [] = new int[8];
+        int start = 1;
+        buildAdjacencyList(edges);
+        if(detectCycleInGraph(start , -1,  visited,adjacencyList))
+            System.out.println("Cycle found");
+
+    }
+}
