212595f1f22fb59d608a0d6b23f9e1ed42ffb301
Satyam Kodale <satyamkodale38@gmail.com>
2026-01-22 12:35:41 +0530

    Solved ATOZ_G-22_Algo_Kahns_Topological_Sort

---
+++ DSA_TOPICS/Graph/Directed_Graph/Algo_Kahns_Topological_Sort.java
@@ -0,0 +1,69 @@
+package Directed_Graph;
+
+import java.util.*;
+
+public class Algo_Kahns_Topological_Sort {
+    static List<List<Integer>> adjacencyList = new ArrayList<>();
+
+
+    public static void buildAdjacencyListAndIndgree(int edges[][] , int n , int [] indgree ){
+
+        for(int i=0; i<n; i++){
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<edges.length; i++){
+            adjacencyList.get(edges[i][0]).add(edges[i][1]);
+            indgree[edges[i][1]]++;
+        }
+
+
+    }
+
+    public static void main(String[] agrs) {
+
+        int edges[][] = {{5, 0}, {4, 0}, {5, 2}, {4, 1}, {2, 3}, {3, 1}};
+
+        int indgree[] = new int[6];
+        buildAdjacencyListAndIndgree(edges, 6 ,indgree);
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        //BFS
+        for(int i=0; i<indgree.length; i++){
+            if(indgree[i]==0) queue.offer(i);
+        }
+
+        if(queue.isEmpty()) {
+            System.out.println(" Graph contains cycle");
+        }
+
+        while(!queue.isEmpty()){
+
+            //pop element from queue
+            int current = queue.poll();
+
+            List<Integer> neighbors = adjacencyList.get(current);
+
+            //explore neighbors , reduce neighbor's indgree and add it to queue if it's indgree becomes zero
+            for(int i=0; i<neighbors.size(); i++){
+
+                int neighbor = neighbors.get(i);
+                indgree[neighbor]--;
+
+                if(indgree[neighbor]==0){
+                    queue.offer(neighbor);
+                }
+            }
+
+            stack.push(current);
+
+        }
+
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+
+
+    }
+}
